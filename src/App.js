b076898e5871fac7c2464441adbcf1666543a585
Chokri Khalifa <khalifa.chokri@gmail.com>
2021-09-22 10:43:29 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+const  App = () => {
+  const clearInput = () => {
+    alert('Hello World');
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, this is a Rusty app! Folks
+        </p>
+        <p>
+          <button onClick={clearInput}>Open</button>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
