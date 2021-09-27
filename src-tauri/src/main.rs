c8777a9fdb945d751db306f8832eaa2ccedde671
Chokri Khalifa <khalifa.chokri@gmail.com>
2021-09-27 16:39:00 +0200

    Send & Receive msg

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,13 @@
   windows_subsystem = "windows"
 )]
 #[tauri::command]
-fn run_command(cmd: String) -> String{
+fn run_command(cmd: String) -> String {
   println!("Msg received {}", cmd);
   return "Hello from Rust".to_string();
 }
 fn main() {
   tauri::Builder::default()
-  .invoke_handler(tauri::generate_handler![run_command])
+    .invoke_handler(tauri::generate_handler![run_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
